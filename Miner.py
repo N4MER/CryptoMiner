9b7db0c45f5450ac61fc21b501bd3f1cc31736b2
N4MER <128246447+N4MER@users.noreply.github.com>
2025-11-25 18:50:41 +0100

    Added Miner class

---
+++ Miner.py
@@ -0,0 +1,22 @@
+class Miner:
+    def __init__(self, username):
+        self._name = username
+        self._wallet = 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value is None or type(value) is not str:
+            raise TypeError("Name must be a string")
+        self._name = value
+
+    @property
+    def wallet(self):
+        return self._wallet
+
+    @wallet.setter
+    def wallet(self, value):
+        self._wallet = value
