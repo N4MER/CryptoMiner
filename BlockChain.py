f412ba7157dd955b870d721118516c7342f02a12
N4MER <128246447+N4MER@users.noreply.github.com>
2025-11-25 18:45:54 +0100

    BlockChain class update

--- BlockChain.py
+++ BlockChain.py
@@ -7,19 +7,48 @@ class Block:
         Constructor for Block class.
         :param previous_hash: Hash of the previous block.
         """
-        self.previous_hash = previous_hash
+        self._previous_hash = previous_hash
         self.reward = 1
-        self.timestamp = time.time()
-        self.nonce = 0
-        self.difficulty = 5
+        self._timestamp = time.time()
+        self._nonce = 0
+        self._difficulty = 5
         self.hash = None
+
+    @property
+    def previous_hash(self):
+        return self.previous_hash
+
+    @previous_hash.setter
+    def previous_hash(self, value):
+        """
+        Setter for previous hash.
+        Value must be a 64-character hex string.
+        :param value:
+        :return:
+        """
+        if value is None or type(value) is not str or len(value) != 64:
+            raise TypeError("Previous hash must be a 64-character hex string.")
+
+        try:
+            int(value, 16)
+        except ValueError:
+            raise TypeError("Previous hash must contain valid hexadecimal characters.")
+
+        self.previous_hash = value
+
+    def increment_nonce(self):
+        """
+        Increments the nonce.
+        :return:
+        """
+        self._nonce += 1
 
     def get_value(self):
         """
         Converts variable values in block to string.
         :return: variable values of the block converted to string.
         """
-        return f"{self.previous_hash} | {self.timestamp} | {self.nonce}".encode()
+        return f"{self.previous_hash} | {self._timestamp} | {self._nonce}".encode()
 
     def hash(self):
         """
@@ -36,7 +65,7 @@ class Block:
         It starts with number of zeros equal to difficulty of the block, and all remaining characters are f.
         :return: True if int value of the block is smaller than int value of the target, and False otherwise.
         """
-        target = ("0" * self.difficulty) + ("f" * (64 - self.difficulty))
+        target = ("0" * self._difficulty) + ("f" * (64 - self._difficulty))
         hash = self.hash()
         if int(hash, 16) < int(target, 16):
             self.hash = hash
@@ -58,4 +87,3 @@ class BlockChain:
         :return:
         """
         self.chain.append(block)
-
